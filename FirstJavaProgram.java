f257b0d80a2bc8b11b614d9f5135a38886a7d2d3
NobleNiraj <gangalenk@gmail.com>
2022-10-22 17:43:05 +0530

    make the class public

--- FirstJavaProgram.java
+++ FirstJavaProgram.java
@@ -1,4 +1,4 @@
-class FirstJavaProgram{
+public class FirstJavaProgram{
     public static void main (String args[]){
         // java does not support the single quotes
         System.out.println("Hello world");
