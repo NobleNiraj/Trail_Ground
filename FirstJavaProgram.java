ed20f905ac042773bbd250091b52b48635389354
NobleNiraj <gangalenk@gmail.com>
2022-10-23 15:43:33 +0530

    add the  FirstJavaProgram.class file

--- FirstJavaProgram.java
+++ FirstJavaProgram.java
@@ -2,6 +2,8 @@ public class FirstJavaProgram{
     public static void main (String args[]){
         // java does not support the single quotes
         System.out.println("Hello world");
+        
+        System.out.println("5"+5);
     
     }
 }
